ad502d72f1939ab2124c94b80d862491325fc35b
nicolai <ngbaranov@gmail.com>
2023-05-24 18:32:13 +0300

    init

---
+++ main.py
@@ -0,0 +1,9 @@
+
+direct_cod = input('Шифрование, дешифрование (y), (n) ')
+lang = input('Язык ввода (ru), (en) ')
+shear_step = int(input())
+text = input('Введите текст для шифрования')
+abc_ru = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+abc_en = 'abcdefghijklmnopqrstuvwxyz'
+
+
